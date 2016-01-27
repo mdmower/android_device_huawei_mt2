458206dd9459588dc8807f259b5692bfc73f310e
Matt Mower <mowerm@gmail.com>
2016-01-26 18:12:24 -0600

    mt2: Update TWRP flags for new minui
    Additionally, enable tap-to-wake.
    
    Change-Id: I69e2d5e487122be59fb5a12e4ebedfaab2a79816

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,14 +113,13 @@ BOARD_SEPOLICY_DIRS += device/huawei/mt2/sepolicy
 
 # TWRP
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_VARIANT := twrp
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
 TW_INCLUDE_CRYPTO := true
+TW_TARGET_USES_QCOM_BSP := true
 TW_THEME := portrait_hdpi
 
 # Vold
